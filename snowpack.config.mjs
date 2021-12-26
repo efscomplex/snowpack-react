357394aeeb5eda04c357bd71014c233e8d3e1329
Edgar <35841638+efscomplex@users.noreply.github.com>
2021-12-26 22:32:57 +0100

    Update snowpack.config.mjs

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -5,6 +5,13 @@ export default {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
+   routes: [
+    {
+      match: 'routes',
+      src: '.*',
+      dest: '/index.html',
+    },
+  ],
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
